e69094e41fcd63133d5c8d60afd6ba926616e093
061672 <elmehdi.hassani@intelcia.com>
2024-11-30 13:32:01 +0100

    add service

--- src/main/java/org/mh/phd/dataextractionservice/DataExtractionServiceApplication.java
+++ src/main/java/org/mh/phd/dataextractionservice/DataExtractionServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DataExtractionServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DataExtractionServiceApplication.class, args);
 	}
 
